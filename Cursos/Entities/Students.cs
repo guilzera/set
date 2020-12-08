8b63c25f72910f74585dde19645a76d6db59ff52
Guilherme Nascimento <guiguidefarias@hotmail.com>
2020-12-08 15:35:55 -0300

    Exercicio HashSet

---
+++ Cursos/Entities/Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cursos.Entities
+{
+    class Students
+    {
+        public int Id { get; set; }
+
+        public Students(int id)
+        {
+            Id = id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Students))
+            {
+                return false;
+            }
+
+            Students other = obj as Students;
+            return Id.Equals(other.Id);
+        }
+    }
+}
---
+++ Cursos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Cursos.Entities;
+
+namespace Cursos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Students> courseA = new HashSet<Students>();
+            HashSet<Students> courseB = new HashSet<Students>();
+            HashSet<Students> courseC = new HashSet<Students>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                courseA.Add(new Students(id));
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                courseB.Add(new Students(id));
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                courseC.Add(new Students(id));  
+            }
+
+            HashSet<Students> all = new HashSet<Students>(courseA);
+            all.UnionWith(courseB);
+            all.UnionWith(courseC);
+            Console.WriteLine("Total Students: " + all.Count);
+
+        }
+    }
+}
